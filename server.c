9e87ead4dece1585df6fee349a3cee8fcaecc0a8
jwestman <johannes.westman89@gmail.com>
2020-11-22 19:26:32 +0100

    initializing

---
+++ server.c
@@ -0,0 +1,20 @@
+/*
+	**********************************************************************************
+	This file implements a server in C.
+
+	Johannes Westman
+	2020-11-xx
+	**********************************************************************************
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+const int port = 1234;
+
+int main(int argc, char *argv[]){
+	
+}
